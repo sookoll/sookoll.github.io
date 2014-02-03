8d3c32c9a5b2c0077d895756be005286ad3f3848
Mihkel Oviir <mihkel.oviir@gmail.com>
2014-02-03 21:42:25 +0200

    up button

--- main.js
+++ main.js
@@ -136,6 +136,22 @@
 			$(this).attr('target','_blank');
 		}
 	});
+	
+	// up
+	var offset = 220;
+	var duration = 500;
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > offset) {
+			$('.back-to-top').fadeIn(duration);
+		} else {
+			$('.back-to-top').fadeOut(duration);
+		}
+	});
+
+	$('.back-to-top').click(function(e) {
+		e.preventDefault();
+		$('html, body').animate({scrollTop: 0}, duration);
+	});
 
 }( window, jQuery ));
 
